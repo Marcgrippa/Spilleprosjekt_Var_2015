178d08e8dd03433ae01f07c128f2199f2a130c02
Marcgrippa <hloekens@hotmail.com>
2015-03-29 14:15:14 +0200

    Endre parameternavn

--- MoveCommand.java
+++ MoveCommand.java
@@ -3,27 +3,27 @@ public class MoveCommand {
 
 	private Location location; 
 	
-	public int goLeft(int x, int y){
-		location.setX(x + 1);
-		location.setY(y);
+	public int goLeft(int oldX, int oldY){
+		location.setX(oldX + 1);
+		location.setY(oldY);
 		return location.getX() + location.getY();
 	}
 	
-	public int goRight(int x, int y){
-		location.setX(x - 1);
-		location.setY(y);
+	public int goRight(int oldX, int oldY){
+		location.setX(oldX - 1);
+		location.setY(oldY);
 		return location.getX() + location.getY();
 	}
 	
-	public int goUp(int x, int y){
-		location.setX(x);
-		location.setY(y - 1);
+	public int goUp(int oldX, int oldY){
+		location.setX(oldX);
+		location.setY(oldY - 1);
 		return location.getX() + location.getY();
 	}
 	
-	public int goDown(int x, int y){
-		location.setX(x);
-		location.setY(y - 1);
+	public int goDown(int oldX, int oldY){
+		location.setX(oldX);
+		location.setY(oldY - 1);
 		return location.getX() + location.getY();
 	}
 	
