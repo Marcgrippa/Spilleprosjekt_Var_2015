c9c2ed34914f7bde0bfc85411edf8ede8bad4554
Marcgrippa <hloekens@hotmail.com>
2015-04-09 14:45:23 +0200

    Room

--- RoomFactory.java
+++ RoomFactory.java
@@ -1,7 +1,13 @@
 /*
- * 1. Tomt rom - EmptyRoom.
- * 2. Søkerom - SearchRoom.
- * 3. 
+ * 1. Tomt rom - EmptyRoom - e
+ * 2. Søkerom - SearchRoom - s
+ * 3. Farerom - DangerRoom - d
+ * 4. ComputerRoom - Lar deg se x antall rom forran deg
+ * 5. SleepingRoom - PowerNapRoom - p - Øker livet ditt med en 
+ * 6. LockedDoor1 - l1 - låst oppover
+ * 7. LockedDoor2 - l2 - låst til høyre
+ * 8. LockedDoor3 - l3 - låst nedover
+ * 9. LockedDoor4 - l4 - låst til venstre
  */
 
 package spillprosjekt;
@@ -24,6 +30,33 @@ public class RoomFactory {
 		for(int k = 0; k < antall[2]; k++){
 			rom.add(new NameRoom("d")); //d for danger.
 		}
+		
+		for(int a = 0; a < antall[3]; a++){
+			rom.add(new NameRoom("c")); // c for computer.
+  		}
+		
+		for(int b = 0; b < antall[4]; b++){
+			rom.add(new NameRoom("p")); // p for powerNap.
+		}
+		
+		for(int c = 0; c < antall[5]; c++){
+			rom.add(new NameRoom("l1")); // l1 for locked_up.
+		}
+		
+		for(int d = 0; d < antall[6]; d++){
+			rom.add(new NameRoom("l2")); // l2 for locked_right.
+		}
+		
+		for(int e = 0; e < antall[7]; e++){
+			rom.add(new NameRoom("l3")); // l3 for locked_down.
+		}
+		
+		for(int f = 0; f < antall[8]; f++){
+			rom.add(new NameRoom("l4")); // l4 for locked_left.
+		}
+		
+		
+		
 	}
 	
 	public ArrayList<NameRoom> getNameRoom(){
